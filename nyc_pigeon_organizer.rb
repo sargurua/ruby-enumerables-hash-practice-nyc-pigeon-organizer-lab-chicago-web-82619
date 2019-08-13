afd49e656e322db1c4960458a3d1421ec5976e4a
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-13 16:08:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, hash|
     hash.each do |attribute, array|
       array.each do |name|
-        if 
+        if new_hash.key?(array[name])
+          
       end
     end
   end
