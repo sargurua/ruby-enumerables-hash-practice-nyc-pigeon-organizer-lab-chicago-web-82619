f85b7fe92c7135ceed2ad47bd945a8e116679150
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-13 16:30:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |key, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if new_hash.key?(name)
+          if new_hash[name].key?(key)
+            new_hash[name][key].push(attribute.to_s)
+          else
+            new_hash[name][key] = [attribute.to_s]
+          end
+        else
+          new_hash[name] = {key => [attribute.to_s]}
+        end
+      end
+    end
+  end
+  new_hash
 end
+
